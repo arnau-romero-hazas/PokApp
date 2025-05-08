a90de5ee7ad885d191a349550672a05238a021a4
alexrocamora <alex.rocamora@estudiantat.upc.edu>
2025-05-08 15:11:33 +0200

    ahora sí que sí #1

--- project/app/logic/users/getUserRolesByIds.js
+++ project/app/logic/users/getUserRolesByIds.js
@@ -1,14 +1,30 @@
-const API_BASE_URL = process.env.API_BASE_URL
+import Constants from 'expo-constants'
+import { data } from '../../data'
+import { errors } from '../../validations'
+
+const API_BASE_URL = Constants.expoConfig.extra.apiBaseUrl
+const { SystemError } = errors
 
 export const getUserRolesByIds = (ids) => {
-  return fetch(`${API_BASE_URL}/users/roles-by-ids`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ids })
-  })
+  return data.getToken()
+    .then(token => {
+      if (!token) throw new SystemError('Token not found')
+
+      return fetch(`${API_BASE_URL}/users/roles-by-ids`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ ids })
+      })
+    })
+    .catch(err => { throw new SystemError(err.message) })
     .then(res => {
-      if (!res.ok) throw new Error('Could not fetch user roles')
-      return res.json()
+      if (res.status === 200) return res.json()
+
+      return res.json().then(body => {
+        throw new SystemError(body.message || 'Error getting user roles')
+      })
     })
 }
-
